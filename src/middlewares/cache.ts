d3382a702103ddd57f462fd824e3718baf7fada8
pengwei <pengwei@antiy.cn>
2018-12-03 09:57:26 +0800

    修改缓存条件设置

--- src/middlewares/cache.ts
+++ src/middlewares/cache.ts
@@ -4,20 +4,20 @@ import { getCache, setCache } from '../utils/cache';
 //本中间件为缓存中间件，只缓存get方法得到的回复
 
 //不需要缓存的url
-const noCacheUrl: Array<string> = ['/test'];
+// const noCacheUrl: Array<string> = ['/test'];
 
-//为true则需要缓存，false则无需缓存
-const check = (url: string) => {
-  let arr = noCacheUrl.map(i => url.substr(0, i.length) === i).filter(i => i);
-  return arr.length === 0;
-};
+// //为true则需要缓存，false则无需缓存
+// const check = (url: string) => {
+//   let arr = noCacheUrl.map(i => url.substr(0, i.length) === i).filter(i => i);
+//   return arr.length === 0;
+// };
 
 
 const cache = async (ctx, next) => {
   let url = ctx.url;
   let method = ctx.request.method;
-  //缓存条件判断
-  if (!check(url) || method !== 'GET') {
+  //非GET方法，不需要缓存
+  if (method !== 'GET') {
     //不需要缓存
     return next();
   }
@@ -31,9 +31,11 @@ const cache = async (ctx, next) => {
     else {
       await next();
       // console.log('设置缓存');
-      let obj = ctx.state && ctx.state.cache_res || null;
+      const objs = ctx.state || {};
+      let obj = objs['cache_res'] || null;
       if (obj) {
-        setCache(url, obj);
+        let config = objs['cache_config'] || {};
+        setCache(config['url'] || url, obj, config['expire'] || Number(process.env.REDIS_EXPIRE));
         return;
       }
     }
